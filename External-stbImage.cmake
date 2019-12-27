6d3c748b43c111f4775b4300281a9e371ac01b18
tmeessen <meessen.thomas@gmail.com>
2019-12-27 23:41:25 +0100

    Adding support in cmake for stb download

---
+++ External-stbImage.cmake
@@ -0,0 +1,18 @@
+message( "External project - GTest" )
+
+ExternalProject_Add(stbImage
+        GIT_REPOSITORY https://github.com/nothings/stb.git
+        GIT_TAG master
+        SOURCE_DIR stb_source
+        UPDATE_COMMAND ""
+        BUILD_COMMAND ""
+        INSTALL_COMMAND ""
+        PATCH_COMMAND ""
+        CONFIGURE_COMMAND ""
+        )
+
+#----
+# Matching the Cmake module FindGtest output variable name to seamlessly switch between the two
+# https://cmake.org/cmake/help/latest/module/FindGTest.html
+
+set( STB_INCLUDE_DIRS ${CMAKE_BINARY_DIR}/stb_source/ )
